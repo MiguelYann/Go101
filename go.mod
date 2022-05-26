0c0b5d5be47dec378d515ecb8ca1ce3e850dc91f
miguel <tsambamiguelito@gmail.com>
2022-05-26 20:54:54 +0200

    Exe shell script on golang

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module example/hello
 
 go 1.18
+
+require (
+	bitbucket.org/creachadair/shell v0.0.7 // indirect
+	github.com/bitfield/script v0.20.1 // indirect
+	github.com/itchyny/gojq v0.12.7 // indirect
+	github.com/itchyny/timefmt-go v0.1.3 // indirect
+)
--- hello.go
+++ hello.go
@@ -2,12 +2,42 @@ package main
 
 import "fmt"
 
+import "github.com/bitfield/script"
+
+
+
 func main() {
 	fmt.Println("Hello World")
 	fmt.Println(add(1, 2))
+
+	aPerson := createPersonWithDefaultHobbies(18, "Yann")
+
+	fmt.Println(aPerson.describePerson())
+
+	executeShellScript();
 }
 
 func add(a, b int) (int) {
 	sum := a + b
 	return sum;
+}
+
+type person struct {
+	name string
+	age int
+	hobbies [2]string
+}
+
+func createPersonWithDefaultHobbies(age int, name string) *person {
+	person := person {age: age, name: name, hobbies: [2]string {"Foot", "Basket"}}
+	return &person
+}
+
+func (p person) describePerson() string {
+	return fmt.Sprintf("My name is %s and I have %d years old" ,  p.name , p.age);
+}
+
+
+func executeShellScript() {
+	script.Echo("Hello from Shell").Stdout()
 }
